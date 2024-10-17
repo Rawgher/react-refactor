d850883274e914474a6492f9dc40c5cea6ff9bef
Rawgher <rdpender3@gmail.com>
2024-10-17 14:54:13 -0700

    code refactored

--- src/Joke.js
+++ src/Joke.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Joke.css";
 
 /** A single joke, along with vote up/down buttons. */
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
+function Joke({id, vote, votes, text}) {
+
+  const upVote = () => vote(id, +1);
+  const downVote = () => vote(id, -1);
 
     return (
       <div className="Joke">
         <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
+          <button onClick={upVote}>
             <i className="fas fa-thumbs-up" />
           </button>
 
-          <button onClick={evt => vote(id, -1)}>
+          <button onClick={downVote}>
             <i className="fas fa-thumbs-down" />
           </button>
 
@@ -25,6 +26,5 @@ class Joke extends Component {
       </div>
     );
   }
-}
 
 export default Joke;
